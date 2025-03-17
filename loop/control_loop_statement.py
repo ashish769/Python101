5ce3069b6589bd4618a4dbcea307e379c1052856
ashish <ashishrajpoudel28@gmail.com>
2025-03-17 20:54:01 +0545

    control loop  updated

---
+++ loop/control_loop_statement.py
@@ -0,0 +1,38 @@
+#control loop statement---> it it used to change the flow of execution or stop the loop
+#pass
+#break
+#continue
+
+# for i in range(10):
+#     print(i)
+
+
+#pass ---it is null statement which is used for future code
+# for i in range(10):
+#     pass
+
+# print("hello siplaya")
+
+
+#break---it is used to terminate the loop when encounter
+# for i in range(10):
+#     print(i)
+#     if i==5:
+#         break
+
+
+#continue: it is used stop the current iteration and continue from next iteration
+
+# for i in range(10):
+#     if i%3==0:
+#         continue
+#     print(i)
+# b=[]
+# a=["sujan","ram",'dipak',"suman","hari"]
+# for i in a:
+#     if i.startswith("s"):
+#         continue
+#     b.append(i)
+    
+# print(b)
+    
