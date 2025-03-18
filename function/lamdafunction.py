dbc261be10f13eee3cc3dd0e90a03dfa55bff1e8
ashish <ashishrajpoudel28@gmail.com>
2025-03-18 20:20:19 +0545

    basic updated

---
+++ function/lamdafunction.py
@@ -0,0 +1,26 @@
+#lambda function -->short and clean code of the function
+#similar to user define function/it is also  called anonymous function/oneline function
+
+#syntax: lambda argument(as long as ):expression
+# def show(m,n):
+#     return n+m
+# print(show(3,2))
+
+# #using lambda function
+# sum=lambda m,n:n+m
+
+# print(sum(2,3))
+
+# #cube of the given number
+
+# cube=lambda n:n**3
+
+# print(cube(5))
+
+
+# def sum(x,y):
+#     print(x+y)
+# print(sum(1,2))
+
+cube=lambda x:x**3
+print(cube(3))
