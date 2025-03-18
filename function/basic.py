b5cb556c6d8b8a780320917fe9ae9b90e9ca5087
ashish <ashishrajpoudel28@gmail.com>
2025-03-18 20:15:38 +0545

    basic updated

---
+++ function/basic.py
@@ -0,0 +1,96 @@
+#function--->block of code which does specific task whenever we call it
+#type of function:
+#mainly two types 
+#built in function  -precode or predefine by developer #function_name()
+#user define function
+
+
+# print("akjsdf"),len(),set(),list(),range(),count(),min(),max(),sum()
+# print(max(min(False,6,9),-6,-4))
+
+# print(sum([3,4,1]))
+
+
+#user define function
+# syntax:
+# def function_name():
+#     #block of code
+
+# function_name() #calling function
+
+# print("hello sipalaya")
+# print(" i want to learn python")
+
+
+# def show():
+#     print("hello sipalaya")
+#     print(" i want to learn python")
+
+# ####
+
+
+# print("imp code is here")
+# show()
+# print("end of code")
+# show()
+
+
+# def show():
+#     a=20
+#     b=30
+#     print(a+b)
+
+# show()
+
+#syntax:
+# def function_name(arg-->formal argument):
+#     #code
+
+# function_name(arg--rgument)
+# def show(x,y):
+#     print("sum of a and b is ",x+y)
+
+# show(2,5)
+# print("some imp code ")
+# show(7,8)
+
+
+
+# a=[1,2,3,4,5]
+# # v=15
+# b=[1,4,5]
+# v=0
+# def summation(a):
+    
+#     for i in a:
+#         v+=i
+
+#     print(v)
+
+# summation(b)
+# summation(a)
+
+
+# return statement -->it is used to exit a function or print the value or result
+# def show(n):
+#     return n*n
+    
+
+# print(show(2))
+
+# print(sum([1,2,3]))
+
+
+# a=[1,2,3,4,5]
+# v=0
+# def show(a):
+#     global v
+#     for i in a:
+#         v+=i
+#     print(v)
+
+# show(a)
+
+
+
+
