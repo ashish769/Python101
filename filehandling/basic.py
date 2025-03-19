4569badcffe6b275e4c8c512193e455aa0250796
ashish <ashishrajpoudel28@gmail.com>
2025-03-19 17:14:09 +0545

    file handling

---
+++ filehandling/basic.py
@@ -0,0 +1,50 @@
+# #file handling
+# #file:: file is used to store data and information permanently
+
+
+# #file types:
+# # two types
+# # text file:eg .txt,.py
+# # binary file:eg jpg,.png,.pdf
+
+# #file handeling
+# # a=input("Enter your name\n:")
+# # f=open("msg.txt",'x')
+# # f.write(a)
+
+
+
+# # syntax
+# # open("filename",'mode')
+
+# # mode.....purpose of opening that file
+# # x=create
+# # r=read
+# # w=write
+# # a=append
+# # w+=write and read
+# # r+=read and write
+
+# # f=open("C:\Users\user\Desktop\jk",'r')
+# # a=f.read()
+# # # print(a)
+# # a=f.readline()
+# # a=f.readlines()
+# # print(a)
+
+
+# # f=open("msg.txt",'w')
+# # f.write
+
+
+# # why file close?
+# #memory westage
+# #data crash
+
+
+
+# try:
+#     f=open("msg.txt",'r')
+
+
+
