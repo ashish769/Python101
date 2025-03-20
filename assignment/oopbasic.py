25e46b2bf527a2e018188c66188dac590dd88df9
ashish <ashishrajpoudel28@gmail.com>
2025-03-20 21:01:31 +0545

    assignment update

---
+++ assignment/oopbasic.py
@@ -0,0 +1,66 @@
+#find the area and perimeter of the circle
+# import math
+# class Circle:
+#     def __init__(self,radius) -> None:
+#         self.radius=radius
+#     def Area(self):
+#         print(f"area of the given circle is {(math.pi*self.radius**2)}")
+#     def Perimeter(self):
+#         print(f"perimeter of the given circle is {2*math.pi*self.radius}")
+        
+
+# rad=int(input("enter the radius of the circle"))
+# obj=Circle(rad)
+# obj.Area()
+# obj.Perimeter()
+
+
+#class to calculate the age of the person
+# import datetime
+
+# print(datetime.time)
+
+# class Person:
+#     def __init__(self,name,country,dob) -> None:
+#         assert dob<2024
+#         self.name=name
+#         self.country=country
+#         self.dob=dob
+#     def calc_age(self):
+#         print(f"{self.name} nationality is {self.country} and age is {datetime.date.today().year-self.dob}")
+        
+# obj=Person("ashish","nepali",2003)
+# obj.calc_age()
+
+#class to do the basic arithmetic operation
+# class Calculator:
+#     def __init__(self,num1,num2) -> None:
+#         self.num1=num1
+#         self.num2=num2
+        
+#     def Add(self):
+#         print(f"Sum of given number is={self.num1+self.num2}")
+#     def Diff(self):
+#         print(f"Diff of given number is={self.num1-self.num2}")
+#     def Mul(self):
+#         print(f"Multiplication of given number is={self.num1*self.num2}")
+#     def Div(self):
+#         print(f"Division of given number is={self.num1/self.num2}")
+#     def F_div(self):
+#         print(f"Floor division of given number is={self.num1//self.num2}")
+        
+# obj=Calculator(24,12)
+# obj.Diff()
+# obj.Add()
+# obj.Div()
+# obj.Mul()
+# obj.F_div()
+
+
+
+        
+        
+
+
+
+        
