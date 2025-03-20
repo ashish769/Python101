f1f7a392e58f924f1d367074cc69365790a15522
ashish <ashishrajpoudel28@gmail.com>
2025-03-20 21:03:11 +0545

    assignment update

---
+++ assignment/condStmt.py
@@ -0,0 +1,62 @@
+# assign a leteer grade based one numerical score
+
+# marks=int(input("enter the marks of the student=>"))
+
+# if marks>= 90:
+#     print("oustanding performance:A+")
+# elif marks>= 80 and marks<90:
+#     print("Excellent performance:A")
+# elif marks>= 70 and marks<80:
+#     print("very good performance:B+")
+# elif marks>= 60 and marks<70:
+#     print("good performance:B")
+# elif marks>= 50 and marks<60:
+#     print("satisfactory performance:C+")
+# elif marks>= 40 and marks<50:
+#     print("Acceptable performance:C")
+# elif marks>= 35 and marks<40:
+#     print("basic performance:d+")
+# else:
+#     print("student is failed")
+
+#accept the age of the 4 people and print the youngest age
+
+# age1=int(input("enter the age of the first person=>"))
+# age2=int(input("enter the age of the second person=>"))
+# age3=int(input("enter the age of the third person=>"))
+# age4=int(input("enter the age of the fourth person=>"))
+
+# if age1<age2 and age1<age3 and age1<age4:
+#     print(f"youngest person age is {age1}")
+# elif age2<age3 and age2<age4 :
+#     print(f"youngest person age is {age2}")
+# elif age3<age4  :
+#     print(f"youngest person age is {age3}")
+# else:
+#     print(f"youngest person age is {age4}")
+    
+#3.check the letter is vowel or not
+
+# vowel=input("enter only one character you want to check =>")
+# if vowel=="a" or vowel=="e" or vowel=="i" or vowel=="o" or vowel=="u":
+#     print("enterd characted is vowel")
+# else:
+#     print("entered character is not vowel")
+    
+# 4.given no is divisible or not by 5
+
+# num=int(input("enter the number"))
+
+# if num%5==0:
+#     print("divisible by 5")
+# else:
+#     print("not divisible by 5")
+
+# 5.user is eligeable for vote or not?
+
+# age=int(input("enter the age =>"))
+
+# if age>=18:
+#     print("user is eligible for vote")
+# else:
+#     print("user is not eligible for vote")
