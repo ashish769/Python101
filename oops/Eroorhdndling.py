7d22468593502eef6a852955fe16b8d54726c5e0
ashish <ashishrajpoudel28@gmail.com>
2025-03-21 22:01:34 +0545

    oop concept

---
+++ oops/Eroorhdndling.py
@@ -0,0 +1,66 @@
+#Exception handling->the process responding unexpected event occur when program is run
+#it help to redue the data crash or data line
+
+# try:
+#     def show(a):
+#         print(a)
+#     show("sujan")
+# except:
+#     print("some error occour in your code")
+# print("some imp code")
+
+#error
+#mainly three type of error
+#syntax error
+#run time error
+
+
+#logical eroor
+# def show():
+#     print(2+3)#you want to see the 6 ans but written code is this
+# show()
+
+
+#rum time error
+#type error
+#dicvided by zero error
+#type error
+#IndexError
+
+# def show(a):
+#     print(a/0)
+#     b="sujan"
+#     print(b[9])
+# show(2)
+
+# try:
+#     def show(a):
+#         print(a/0)
+#         b="sujan"
+#         print(b[9])
+#     show(2)
+# except ZeroDivisionError:
+#     print("you divide the any number by zero")
+# except TypeError:
+#     print("you add the different data type")
+# print("some imp code")
+
+
+# try:
+#     def show(a):
+#         print(a/0)
+#         b="sujan"
+#         print(b[9])
+#     show(2)
+# except ZeroDivisionError:
+#     print("you divide the any number by zero")
+# except TypeError:
+#     print("you add the different data type")
+#     print("some imp code")#it didnot execute if except 
+    
+# finally:
+#     print("hello sipalaya")
+
+
+
+    
